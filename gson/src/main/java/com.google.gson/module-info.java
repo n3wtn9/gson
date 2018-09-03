2ff24418d2b509f4bda59876b594620ac42fc142
Newton Truong <newtontruong@thrivemarket.com>
2018-09-03 15:10:39 -0700

    remove java modules

--- gson/src/main/java/com.google.gson/module-info.java
+++ gson/src/main/java/com.google.gson/module-info.java
@@ -1,8 +1,8 @@
-module com.google.gson {
-	exports com.google.gson;
-	exports com.google.gson.annotations;
-	exports com.google.gson.reflect;
-	exports com.google.gson.stream;
-
-	requires java.sql;
-}
+//module com.google.gson {
+//	exports com.google.gson;
+//	exports com.google.gson.annotations;
+//	exports com.google.gson.reflect;
+//	exports com.google.gson.stream;
+//
+//	requires java.sql;
+//}
